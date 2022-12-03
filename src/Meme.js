09ab1905bb58d3a9160fbea4a7eac7932fe43f48
AMT98 <aswinmalla12@gmail.com>
2022-12-03 14:15:28 -0700

    Add state

--- src/Meme.js
+++ src/Meme.js
@@ -1,10 +1,26 @@
 import MemesData from "./MemesData"
+import React, { useState } from 'react';
+
 export const Meme = () => {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText : "",
+        randomImage : "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = useState(MemesData)
+
     const handleMemeBtn = () => {
-        const memesArray = MemesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
-        console.log(url);
+        
+        setMeme((prevState => {
+            return {
+                ...prevState ,
+                randomImage : url 
+
+            }
+        }))
     }
     return (
         <main className="meme">
@@ -12,7 +28,8 @@ export const Meme = () => {
                 <input 
                     type="text"
                     placeholder="Top text"
-                    className="form--input"
+                    className="form--input" 
+                    // {meme.topText}
                 />
                 <input 
                     type="text"
@@ -26,6 +43,7 @@ export const Meme = () => {
                 >
                     Get a new meme image 🖼
                 </button>
+                <img className="memeImage" src={meme.randomImage}  alt="memeImage"/>
         </main>
     )
 }
